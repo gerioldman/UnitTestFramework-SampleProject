16ef98b25d6824e01f259258dfb5ea2610cc4b2d
gerioldman <k.gergo49@gmail.com>
2022-11-21 15:29:30 +0100

    minor fix to UnitTestRunner.mk

--- UnitTestRunner/UnitTestRunner.mk
+++ UnitTestRunner/UnitTestRunner.mk
@@ -2,7 +2,7 @@
 # Unit build for unit test
 ###########################
 
-FOUNDSTRING = $(findstring unittest_x86_x64_build,$(MAKECMDGOALS)) $(findstring unittest_platform_build,$(MAKECMDGOALS))
+FOUNDSTRING = $(findstring unittest_x86_x64_build,$(MAKECMDGOALS))$(findstring unittest_platform_build,$(MAKECMDGOALS))
 
 ifneq (,$(FOUNDSTRING))
 
